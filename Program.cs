c67cda22a3821bc2beb924c879738f3d017ec857
Adrien Dion <adion81@gmail.com>
2020-08-03 15:36:18 -0500

    Boxing Method

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Boxing();
+        }
+
+
+        static void Boxing()
+        {
+            object number = 5;
+            int sum = 0;
+            for(int i = 0; i < 10; i++)
+            {
+                sum += (int)number;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
